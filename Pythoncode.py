829e41aaf4a43767147ebf49141297a0070f63da
NayanSubedi <51120587+NayanSubedi@users.noreply.github.com>
2023-01-13 22:50:56 +0545

    Updated Python code with Output
    Output is commented on code

--- Pythoncode.py
+++ Pythoncode.py
@@ -59,3 +59,50 @@ print("ModelName:", lamborghini.ModelName)
 print("engine_capacity:", lamborghini.Engine_capacity)
 print("topSpeed:", lamborghini.TopSpeed)
 print("horsepower:", lamborghini.Horsepower)
+
+
+
+# What color is your Bugatti? : Copper
+# Enter the OwnerName of Bugatti : Andrew Tate ( Top G)
+# Enter the model name of Bugatti: Chiron
+# Enter the Engine capacity of Bugatti: 8.0 L quad-turbocharge W16 engine
+# Enter the TopSpeed of Bugatii in KM/H: 305
+# Enter the horsepower of Bugatti: 1479
+
+# Brand: Bugatti
+# Color: Copper
+# OwnerName: Andrew Tate ( Top G)
+# ModelName: Chiron
+# Engine_capacity: 8.0 L quad-turbocharge W16 engine
+# TopSpeed: 305
+# Horsepower: 1479
+
+# What color is your Ferrari?: Red
+# Enter the OwnerName of Bugatti : Nayan
+# Enter the model name of Ferrari: 812 SuperFast
+# Enter the Engine capacity of Ferrari: 6.5-liter V-12
+# Enter the TopSpeed of Ferrari in KM/H: 340
+# Enter the horsepower of Ferrari: 789
+
+# Brand: Ferrari
+# Color: Red
+# OwnerName: Nayan
+# ModelName: 812 SuperFast
+# Engine_capacity: 6.5-liter V-12
+# TopSpeed: 340
+# Horsepower: 789
+
+# Enter the color of Lamborghini: white
+# Enter the OwnerName of Lamborghini : Nayan Subedi
+# Enter the model name of Lamborghini: Venono
+# Enter the Engine capacity of Lamborghini: TYPEV12, 60°, MPI (Multi Point Injection)
+# Enter the TopSpeed of Lamborghini in KM/H: 335
+# Enter the Horsepower of Lamborghini: 750
+
+# Brand: Lamborghini
+# Color: white
+# OwnerName: Nayan Subedi
+# ModelName: Venono
+# engine_capacity: TYPEV12, 60°, MPI (Multi Point Injection)
+# topSpeed: 335
+# horsepower: 750
